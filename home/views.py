848df3fdc7b2ed5c05c9b18ebe68b5f04dbbbaea
Arya677 <116159491+Arya677@users.noreply.github.com>
2025-08-12 10:28:59 +0530

    27323

--- home/views.py
+++ home/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
 
 def homepage(request):
-    return render(request, 'home/index.html')
+    restaurant = Restaurant.objects.first()
+    if restaurant and restaurant.name:
+        restaurant_name = restaurant.name
+    else:
+        restaurant_name = getattr(settings, 'RESTAURANT_NAME', 'Restaurant')
+    return render(request, 'home/index.html',{'restaurant_name': restaurant_name})
