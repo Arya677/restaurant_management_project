1cf0dc13ab6fd423f222a0800d0c168ebda43f53
Arya677 <116159491+Arya677@users.noreply.github.com>
2025-08-16 23:36:11 +0530

    33433

--- home/models.py
+++ home/models.py
@@ -1,7 +1,5 @@
-from dajngo.db import models
+from django.db import models
 
-class Restaurant(models.Model):
-    name = models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.name
+class Restaurnat(models.Model):
+    name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
