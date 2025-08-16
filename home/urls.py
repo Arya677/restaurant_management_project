949e383451e13d66a26d20b02caab11b54200865
Arya677 <116159491+Arya677@users.noreply.github.com>
2025-08-17 00:02:06 +0530

    33430

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     path('', views.homepage, name='homepage'),
     path('about/', views.restaurant_about, name="restaurant_about"),
+    path('menu/', views.menu_list, name="menu_list"),
 ]
