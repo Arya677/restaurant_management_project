d301ebe71ec7a7ed588a264d94ca0a2f76c3567e
Arya677 <116159491+Arya677@users.noreply.github.com>
2025-08-22 16:47:07 +0530

    37732

--- home/urls.py
+++ home/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('about/', views.restaurant_about, name="restaurant_about"),
     path('menu/', views.menu_list, name="menu_list"),
     path('contact/', views.contact_us, name='contact_us'),
+    path('feedback/', views.feedback_view, name='feedback'),
+
 ]
