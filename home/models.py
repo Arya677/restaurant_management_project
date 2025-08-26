8e6a2c384046fce568ff6be3f78adb844fd75e94
Arya677 <116159491+Arya677@users.noreply.github.com>
2025-08-26 23:59:52 +0530

    39037

--- home/models.py
+++ home/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    phone_numer = models.CharField(max_length=15, blank=True,)
+
+    def __str__(self):
+        return self.user.get_full_name() or self.user.username
+
 class Order(models.Model):
     STATUS_CHOICES = [
         ('pending', 'Pending'),
