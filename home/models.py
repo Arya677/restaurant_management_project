c601c73279b5f36ea9ee4204ca3db446cebd82f5
Arya677 <116159491+Arya677@users.noreply.github.com>
2025-08-26 23:53:32 +0530

    39039

--- home/models.py
+++ home/models.py
@@ -1,4 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Order(models.Model):
+    STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('confirmed', 'Confirmed'),
+        ('preparing', 'preparing'),
+        ('completed', 'Completed'),
+        ('cancelled', 'Cancelled'),
+    ]
+
+    customer = models.ForeignKey(User, on_delete=models.CASCADE, related_name ='orders')        
+    total_amount = models.DecimalField(max_digits=10, decimal_places = 2, default=0.00)
+    status = models.CharField(max_length = 20, choices = STATUS_CHOICES, default = 'pending')
+    created_at = models.DateTimeField(auto_now_add =True)
+
+    def __str__(self):
+        return f"Order #{self.id} by {self.customer.username}"
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE,related_name="items" )
+    menu_item = models.ForeignKey('menu', on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity} x {self.menu_item.name} (Order #{self.order.id})"
+
+    def get_item_total(self):
+        return self.menu_item.price * self.quantity    
+
 
 class Restaurnat(models.Model):
     name = models.CharField(max_length=100)
