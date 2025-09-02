3ff28dcde5d7c933b69a0357278350ef93364457
Arya677 <116159491+Arya677@users.noreply.github.com>
2025-09-02 11:27:25 +0530

    41384

--- home/models.py
+++ home/models.py
@@ -12,7 +12,7 @@ class MenuItem(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=8, decimal_places=2)
-
+    image = models.ImageField(Upload_to='menu_images/',blank=True, null=True)                                        
     def __str__(self):
         return f"{self.name} = {self.price}"
 
