df434c02fc7dc2dd7fd591a407b29e1cd6dc9777
Arya677 <116159491+Arya677@users.noreply.github.com>
2025-09-02 11:36:30 +0530

    41387

--- home/models.py
+++ home/models.py
@@ -47,6 +47,9 @@ class OrderItem(models.Model):
 
 class Restaurnat(models.Model):
     name = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    zip_code = models.CharField(max_length=20)
     phone_number = models.CharField(max_length=15, blank=True, null=True)
     address = models.CharField(max_length=225,blanl=True)
 
