f07a58e26a936ed572b9a50dbcedd055036a2938
Arya677 <116159491+Arya677@users.noreply.github.com>
2025-09-02 11:35:00 +0530

    41387

--- home/urls.py
+++ home/urls.py
@@ -9,4 +9,4 @@ urlpatterns = [
     path('feedback/', views.feedback_view, name='feedback'),
     path('api/', include("rest.urls")),
 ] if settings.DEBUG:
-      urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)                            )
+      urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)                                  )
