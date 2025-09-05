126cb6f99c39d7c57e497991c430ca26cfdef0e4
Arya677 <116159491+Arya677@users.noreply.github.com>
2025-09-05 11:38:23 +0530

    42704

--- restaurant_management/settings.py
+++ restaurant_management/settings.py
@@ -124,3 +124,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_PORT_TLS = True
+EMAIL_HOST_USER = "arpit.arya.568@gmail.com"
+EMAIL_HOST_PASSWORD = ""
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
