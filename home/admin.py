f3230f5a9962400e70b915807ac3a98d1a915120
Arya677 <116159491+Arya677@users.noreply.github.com>
2025-09-08 09:46:40 +0530

    42705

--- home/admin.py
+++ home/admin.py
@@ -3,4 +3,7 @@ from .models import Menu, Order, MenuItem
 
 admin.site.register(Menu)
 admin.site.register(Order)
-admin.site.register(MenuItem)
+
+@admin.register(MenuItem)
+class MenuItemAdmin(admin,ModelAdmin):
+    list_display = ('name','price','image')
