06dae82ccfbbf24d0cebb875bedde8d0dd9f5fd3
Arya677 <116159491+Arya677@users.noreply.github.com>
2025-09-09 21:16:16 +0530

    43367

--- home/forms.py
+++ home/forms.py
@@ -14,3 +14,9 @@ class ContactForm(forms.ModelForm):
         model = ContactSubmission
         fields = ['name','email']
         message = forms.CharField(widgets=forms.Textarea)
+
+        def clean_message(self):
+            message = self.cleaned_data.get("message")
+            if len(message.strip()) < 10:
+                raise forms.ValidationError("Message must be at least 10 character long")
+            return message
