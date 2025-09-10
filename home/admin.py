707df384f03a77c7f82cf9bf175cde9cb34bbcf9
Arya677 <116159491+Arya677@users.noreply.github.com>
2025-09-10 15:48:01 +0530

    43371

--- home/admin.py
+++ home/admin.py
@@ -10,4 +10,4 @@ class MenuItemAdmin(admin,ModelAdmin):
 
 @admin.register(Restaurant):
 class RestaurantAdmin(admin.ModelAdmin):
-    list_display = ('name','address')
+    list_display = ('name','address','phone_number')
