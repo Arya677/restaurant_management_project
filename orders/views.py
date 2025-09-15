570d9c2b2bea23d415d6114d9f2f3976c6fcf91e
Arya677 <116159491+Arya677@users.noreply.github.com>
2025-09-15 18:06:03 +0530

    44417

--- orders/views.py
+++ orders/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
 
 def order_page(request):
-    return render(request, "order.html")
+    return render(request, "order.html
+
+def order_confirmation(request):
+    order_number = random.random.randit(1000.99999)
+    return render(request, "order_confirmation.html", {"order_number": order_number})
