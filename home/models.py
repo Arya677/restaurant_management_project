953decd165bdee65c3e7a841bbf61428b4f9448f
Arya677 <116159491+Arya677@users.noreply.github.com>
2025-09-16 12:13:47 +0530

    45058

--- home/models.py
+++ home/models.py
@@ -11,11 +11,16 @@ class UserProfile(models.Model):
 class MenuCategory(models.Model):
     name = models.CharField(max_length=100, unique=True)
 
+    def __str__(self):
+        return self.name
+
 class MenuItem(models.Model):
+    category = models.ForeignKey(MenuCategory, on_delete=models.CASCADE, related_name="items", null="True", blank="True")          
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     image = models.ImageField(Upload_to='menu_images/',blank=True, null=True)                                        
+    
     def __str__(self):
         return f"{self.name} = {self.price}"
 
