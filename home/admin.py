3aa7cd23d4b7e81deae2f8a206b5686ac08dd026
Arya677 <116159491+Arya677@users.noreply.github.com>
2025-09-16 12:15:07 +0530

    45058

--- home/admin.py
+++ home/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Menu, Order, MenuItem, Restaurant
+from .models import Menu, Order, MenuItem, Restaurant, UserProfile, MenuCategory               
 
 admin.site.register(Menu)
 admin.site.register(Order)
+admin.site.register(MenuCategory)
+admin.site.register(UserProfile)
 
 @admin.register(MenuItem)
 class MenuItemAdmin(admin,ModelAdmin):
