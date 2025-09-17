18081dad5601f9cce8d4dc40ae687724d9951e16
Arya677 <116159491+Arya677@users.noreply.github.com>
2025-09-17 15:46:28 +0530

    45057

--- orders/models.py
+++ orders/models.py
@@ -1,5 +1,10 @@
 from django.db import models
+from .status_models import OrderStatus
 
 class Order(models.Model):
     customer_name = models.CharField(max_length=100)
     order_date = models.DateTimeField(auto_now_add=True)
+    status = models.ForeignKey(OrderStatus, on_delete = models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f"Order #{self.id} - {self.customere_name}"      
