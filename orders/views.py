b1176057f8e964a4c1e9271214ed99f8dc059ac3
Arya677 <116159491+Arya677@users.noreply.github.com>
2025-09-17 15:41:57 +0530

    45057

--- orders/views.py
+++ orders/views.py
@@ -6,3 +6,5 @@ def order_page(request):
 def order_confirmation(request):
     order_number = random.random.randit(1000.99999)
     return render(request, "order_confirmation.html", {"order_number": order_number})
+
+         
