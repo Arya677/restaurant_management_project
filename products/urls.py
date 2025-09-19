bcf8b0ecf880535349b06df693593d7e1190d0c1
Arya677 <116159491+Arya677@users.noreply.github.com>
2025-09-19 17:52:22 +0530

    48542

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from .views import *
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+
 
 urlpatterns = [
-    path('items/', ItemView.as_view(), name='item-list'),
+    path('items/', ItemView.as_view(), name='item-list'),                               
     path("api/menu/category/", menu_items_by_category, name='menu-items-by-cayegpory')
+    path("api/menu/search", MenuItemSearchViewSet, name="menu-item-search" )
+]
