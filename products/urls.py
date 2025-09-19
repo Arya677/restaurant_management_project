55ec16e1edd8cdaee4fee7f657f35fc0e39898c0
Arya677 <116159491+Arya677@users.noreply.github.com>
2025-09-19 17:05:15 +0530

    48545

--- products/urls.py
+++ products/urls.py
@@ -3,4 +3,4 @@ from .views import *
 
 urlpatterns = [
     path('items/', ItemView.as_view(), name='item-list'),
-]
+    path("api/menu/category/", menu_items_by_category, name='menu-items-by-cayegpory')
