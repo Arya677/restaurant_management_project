580c0a0047f0b970fd5e23618d90bfd88a3f40bc
Arya677 <116159491+Arya677@users.noreply.github.com>
2025-09-19 16:39:14 +0530

    48544

--- account/views.py
+++ account/views.py
@@ -1,2 +1,7 @@
 from django.shortcuts import render
+from restaurant_management.utils.validation_utils import is_valid_email  
 
+def regester_user(request):
+    email = request.POST.get("email")
+    if not is_valid_email(email):
+        return JsonResponse({"error": "Invalid email"}, status=400)
