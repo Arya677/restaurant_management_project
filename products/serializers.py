f1e3dfcd546ab60b721899cfd523f88d0a803a0c
Arya677 <116159491+Arya677@users.noreply.github.com>
2025-09-19 16:57:14 +0530

    48545

--- products/serializers.py
+++ products/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
-from .models import Item
+from .models import Item, MenuItem
 
 class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
         fields = '__all__'
+
+class MenuItenSerializer(serializers.ModelSerializer):
+    category_name = serializers.CharField(source="categoty.name",read_only=True) 
+
+    class Meta:
+        model = MenuItem
+        fields = "__all__"      
+  
