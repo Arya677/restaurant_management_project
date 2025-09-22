217f196849506fe574fb8f09180f2afaf4c38b28
Arya677 <116159491+Arya677@users.noreply.github.com>
2025-09-22 12:20:55 +0530

    48543

--- products/models.py
+++ products/models.py
@@ -7,9 +7,10 @@ class Category(models.Models):
         return self.name
 
 class Item(models.Model):
-    item_name = models.CharField(max_length=150)
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null = True)
     item_price = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return str(self.item_name)
+        return str(self.name)
