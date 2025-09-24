692e47c05acf0f6f454f84f330debfb3fd034b46
Arya677 <116159491+Arya677@users.noreply.github.com>
2025-09-24 11:27:06 +0530

    50283

--- orders/models.py
+++ orders/models.py
@@ -1,10 +1,20 @@
 from django.db import models
 from .status_models import OrderStatus
 
+class ActiveOrderManager(models.manager):
+    def get_active_orders(self):
+        return self.filter(status__in=['pending','processing'])
+
 class Order(models.Model):
+    STATUS_CHOICES = [
+        ('pending',"Pending"),
+        ('processing', 'Processing'),
+        ('completed', 'Completed'),
+        ('cancelled', 'Cancelled'),
+    ]
     customer_name = models.CharField(max_length=100)
-    order_date = models.DateTimeField(auto_now_add=True)
-    status = models.ForeignKey(OrderStatus, on_delete = models.SET_NULL, null=True)
+    order_date = models.DateTimeField(auto_now_add=True)                                       
+    status = models.ForeignKey(OrderStatus, choices=STATUS_CHOICES, on_delete = models.SET_NULL, null=True)
 
     def __str__(self):
         return f"Order #{self.id} - {self.customere_name}"
