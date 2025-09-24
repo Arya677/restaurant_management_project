de9ad25579c4f3156ceb3fe7d5999e30ffb65c2a
Arya677 <116159491+Arya677@users.noreply.github.com>
2025-09-24 12:11:56 +0530

    50281

--- account/views.py
+++ account/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 from restaurant_management.utils.validation_utils import is_valid_email  
+from django.contrib.auth.models import User
+from rest_framework import generics, permissions
+from .serializers import UserUpdateSerializer
+
+class UserProfileUpdateView(generics.UpdateAPIView):
+    serializer_class = UserUpdateSerializer
+    permissions_class = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
 
 def regester_user(request):
     email = request.POST.get("email")
