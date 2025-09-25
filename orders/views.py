d4c341f611688ec1b1d63d8522de79bc5340088c
Arya677 <116159491+Arya677@users.noreply.github.com>
2025-09-25 15:56:29 +0530

    51184

--- orders/views.py
+++ orders/views.py
@@ -17,6 +17,18 @@ class OrderHistoryView(APIView):
 def order_page(request):
     return render(request, "order.html
 
+def create_order(request):
+    if request.method == 'POST':
+        customer_name = request.POST.get("customer_name")
+        total_amount = request.POST.get('total_amount')
+
+        order = Order.objects.created(
+            customer_name = customer_name,
+            total_amount=total_amount
+        )
+        return redirect("order_detail",order_id=order.order_id) 
+    return render(request, "orders/create_order.html")  
+   
 def order_confirmation(request):
     order_number = random.random.randit(1000.99999)
     return render(request, "order_confirmation.html", {"order_number": order_number})
