9e73cfa5fdd66aed646f80bbcbeb8f351f9ea1de
Arya677 <116159491+Arya677@users.noreply.github.com>
2025-09-25 15:59:13 +0530

    51184

--- orders/models.py
+++ orders/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from .status_models import OrderStatus
+from .utils import generate_unique_order_id
 
 class ActiveOrderManager(models.manager):
     def get_active_orders(self):
@@ -15,6 +16,12 @@ class Order(models.Model):
     customer_name = models.CharField(max_length=100)
     order_date = models.DateTimeField(auto_now_add=True)                                       
     status = models.ForeignKey(OrderStatus, choices=STATUS_CHOICES, on_delete = models.SET_NULL, null=True)
+    total_amount = models.CharField(max_digits=10, decimal_places=2)
+    
+    def save(self, *args, **kwargs):
+        if not self.order_id:
+            self.order_id = generate_unique_order_id)
+        super().save(*args,**kwargs)
 
     def __str__(self):
         return f"Order #{self.id} - {self.customere_name}"
