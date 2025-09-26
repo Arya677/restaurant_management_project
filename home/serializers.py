e2be10b5bf5385cf3fc7a98ac46c9ce2fdf708d7
Arya677 <116159491+Arya677@users.noreply.github.com>
2025-09-26 08:02:52 +0530

    51185

--- home/serializers.py
+++ home/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import MenuCategory
+from .models import MenuCategory, ContactSubmission
 
 class MenuCategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = MenuCategory
         fields = ['id','name']
+
+class ContactSubmissionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ContactSubmission
+        fields = "__all__"
