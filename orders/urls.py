720f5f94959debbd6d69bb2ac4207b82c1c28a90
Arya677 <116159491+Arya677@users.noreply.github.com>
2025-09-26 08:31:15 +0530

    51187

--- orders/urls.py
+++ orders/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
 from .views import *
 
+router = DefaultRouter()
+router.register(r'orders', OrderViewSet, basename='orders')                  
+  
 urlpatterns = [
-    path('order/', views.order_page, name="order"),  
+    path('order/', views.order_page, name="order"),         
     path('<str:order_id/', OrderDeatilView.as_view(), name= "order-detail"),      
     path('api/orders/history/', OrderHistoryView.as_view(), name= "order-history"),
+    path('api/', include(router.urls)),
 ]
