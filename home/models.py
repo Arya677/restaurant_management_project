4d8887494ba44c52423841b1c581f4816d0dc9af
Arya677 <116159491+Arya677@users.noreply.github.com>
2025-09-29 12:29:57 +0530

    51488

--- home/models.py
+++ home/models.py
@@ -20,6 +20,7 @@ class MenuItem(models.Model):
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     image = models.ImageField(Upload_to='menu_images/',blank=True, null=True)                                        
+    is_daily_special = models.BooleanField(default=False)
     
     def __str__(self):
         return f"{self.name} = {self.price}"
