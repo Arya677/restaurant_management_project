a8694e4ee0c769719a6722713d5b3b3f3ef38d6e
Arya677 <116159491+Arya677@users.noreply.github.com>
2025-10-08 11:48:22 +0530

    53853

--- home/utils.py
+++ home/utils.py
@@ -2,6 +2,7 @@ from django.core.mail import send_mail, BadHeaderError
 from django.core.exceptions import ValidationError
 from django.core.validators import validate_email
 from django.conf import settings
+from datetime import datetime, time
 
 def send_mail(recipient, subject, message):
     try:
@@ -21,3 +22,25 @@ def send_mail(recipient, subject, message):
         return {'status':'error','message':'Invalid header found'}
     except Exception as e:
         return {'status':'error','message':str(e)}
+
+def is_restaurant_open():
+    now = datetime.now()
+    currtent_day = now.weekday()
+    current_time = now.time()
+
+    opening_hours = {
+        0: (time(9, 0), time(22, 0)),
+        1: (time(9, 0), time(22, 0)),
+        2: (time(9, 0), time(22, 0)),
+        3: (time(9, 0), time(22, 0)),
+        4: (time(9, 0), time(22, 0)),
+        5: (time(9, 0), time(23, 0)),
+        6: (time(9, 0), time(23, 0)),
+    }
+
+    open_time, close_time = opening_hours.get(current_day)
+
+    if open_time <= current_time <-= close_time:
+        return True
+    else:
+        return False
