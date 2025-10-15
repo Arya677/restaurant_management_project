95ac6519bdc8ac39fa44d057f65c3c17c575bfbc
Arya677 <116159491+Arya677@users.noreply.github.com>
2025-10-15 09:12:45 +0530

    54993

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    
     path('', views.homepage, name='homepage'),
     path('about/', views.restaurant_about, name="restaurant_about"),
     path('menu/', views.menu_list, name="menu_list"),
